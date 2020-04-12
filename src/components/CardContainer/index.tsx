2ef3a00d317c286746a3130b6fd163b48a8cc9e7
jevvilla <juan.es-19@hotmail.com>
2020-04-12 07:50:18 -0500

    characters is being looped in the CardContainer component in order to render all cards

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import { Card } from '../Card';
+import { characters } from '../../assets/data';
+import { Character } from '../../common/types';
 
 import './styles.scss';
 
 export const CardContainer = () => {
   return (
     <section className="vote-section-component">
-      <Card />
+      {characters.map((item: Character) => (
+        <Card key={item.id} item={item} />
+      ))}
     </section>
   );
 };
