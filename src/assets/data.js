ecb449d30bd9e8a51b4cf7c4fa8495f665f4920d
jevvilla <juan.es-19@hotmail.com>
2020-04-12 07:41:35 -0500

    data.js is created to export a list of characters that are going to be voted by user

---
+++ src/assets/data.js
@@ -0,0 +1,55 @@
+export const characters = [
+  {
+    id: 1,
+    imageUrl: 'https://images.wsj.net/im-168406?width=1280&size=1',
+    name: 'Kanye West',
+    category: 'Entertainment',
+    createdAt: '1 month ago',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed tenetur placeat.',
+    votes: {
+      up: 320,
+      down: 215,
+    },
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://s1.eestatic.com/2018/03/20/mundo/Facebook-Mark_Zuckerberg-Mundo_293484042_70725138_1706x1280.jpg',
+    name: 'Mark Zuckerberg',
+    category: 'Business',
+    createdAt: '1 month ago',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed tenetur placeat.',
+    votes: {
+      up: 70,
+      down: 55,
+    },
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://personajeshistoricos.com/wp-content/uploads/2018/06/cristina-fernandez-de-kirchner-2.jpg',
+    name: 'Cristina Fernández de Kirchner',
+    category: 'Politics',
+    createdAt: '3 month ago',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quo quam fuga quibusdam.',
+    votes: {
+      up: 32,
+      down: 100,
+    },
+  },
+  {
+    id: 4,
+    imageUrl:
+      'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/malala_yousafzai_free_0.jpeg?td3vC0oWH0R.4wo3K.9m_YwySki1V8TF',
+    name: 'Malala Yousafzai',
+    category: 'Entertainment',
+    createdAt: '1 month ago',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quo quam fuga quibusdam.',
+    votes: {
+      up: 120,
+      down: 32,
+    },
+  },
+];
