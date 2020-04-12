3cc4e78b1273f2592d4bdb0e12261332f5923f0c
jevvilla <juan.es-19@hotmail.com>
2020-04-12 17:24:10 -0500

    Home screen styles updated and footer created and styles added also

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,22 @@ export const Home = () => {
   return (
     <div className="home-container">
       <CardContainer />
+      <div className="footer">
+        <ul>
+          <li>Terms and Conditions</li>
+          <li>Privacy Policy</li>
+          <li>Contact Us</li>
+        </ul>
+        <ul>
+          <li>Follow Us</li>
+          <li>
+            <i className="fa fa-facebook-square"></i>
+          </li>
+          <li>
+            <i className="fa fa-twitter"></i>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
