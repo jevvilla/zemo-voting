1d765a49a1b62ac214241d929b2353fb511ad02b
jevvilla <juan.es-19@hotmail.com>
2020-04-13 15:29:33 -0500

    Header component updated, now using reposiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,25 @@ import './styles.scss';
 
 export const Header = () => {
   return (
-    <div className="header-container">
-      <h1>Rule of Thumb</h1>
-      <ul>
-        <li>Past Trials</li>
-        <li>How It Works</li>
-        <li>Log In / Sign Up</li>
-        <li>
-          <i className="fa fa-search"></i>
-        </li>
-      </ul>
-    </div>
+    <header className="header">
+      <nav className="header__navigation">
+        <div className="header__navigation-logo">
+          <h1>Rule of Thumb</h1>
+        </div>
+        <div className="header__navigation-bars">
+          <i className="fa fa-bars"></i>
+        </div>
+        <div className="header__navigation-items">
+          <ul>
+            <li>Past Trials</li>
+            <li>How It Works</li>
+            <li>Log In / Sign Up</li>
+            <li>
+              <i className="fa fa-search"></i>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
