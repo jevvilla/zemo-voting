5b1784ec7232b96768d820fc3ddbf68b652777d7
jevvilla <juan.es-19@hotmail.com>
2020-04-13 10:01:57 -0500

    banner created and Header component integrated inside Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 
-import { CardContainer } from '../../components';
+import { CardContainer, Header } from '../../components';
 
 import './styles.scss';
 
 export const Home = () => {
   return (
     <div className="home-container">
-      <CardContainer />
-      <div className="footer">
-        <ul>
-          <li>Terms and Conditions</li>
-          <li>Privacy Policy</li>
-          <li>Contact Us</li>
-        </ul>
-        <ul>
-          <li>Follow Us</li>
-          <li>
-            <i className="fa fa-facebook-square"></i>
-          </li>
-          <li>
-            <i className="fa fa-twitter"></i>
-          </li>
-        </ul>
+      <div className="home-banner">
+        <Header />
+      </div>
+      <div className="home-voting">
+        <CardContainer />
+        <div className="footer">
+          <ul>
+            <li>Terms and Conditions</li>
+            <li>Privacy Policy</li>
+            <li>Contact Us</li>
+          </ul>
+          <ul>
+            <li>Follow Us</li>
+            <li>
+              <i className="fa fa-facebook-square"></i>
+            </li>
+            <li>
+              <i className="fa fa-twitter"></i>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
