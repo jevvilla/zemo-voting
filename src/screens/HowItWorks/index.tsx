91c29c3deee5bd24753c855fd85eb1aac3ba096a
jevvilla <juan.es-19@hotmail.com>
2020-04-13 18:48:58 -0500

    HowitWorks screen added to navigate to it

---
+++ src/screens/HowItWorks/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.scss';
+
+export const HowItWorks = () => {
+  return (
+    <div className="howitworks-screen">
+      <span>HowItWorks</span>
+    </div>
+  );
+};
---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+import { Home } from './Home';
+import { HowItWorks } from './HowItWorks';
+
+export { Home, HowItWorks };
