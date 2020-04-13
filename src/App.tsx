4511abf1ef4f773c7b3b3452834043adc2abedaa
jevvilla <juan.es-19@hotmail.com>
2020-04-13 15:30:04 -0500

    App entry point reestructured and styles updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { Home } from './screens/Home';
+import { Header, Footer } from './components';
 
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   );
 };
